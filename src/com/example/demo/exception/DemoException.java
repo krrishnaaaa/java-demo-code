f47192cfd86c111ee98786713a4a5b3a6a0a6a90
krrishnaaaa <krishna@pcsalt.com>
2016-09-12 23:37:32 +0530

    checking return from finally block

---
+++ src/com/example/demo/exception/DemoException.java
@@ -0,0 +1,32 @@
+package com.example.demo.exception;
+
+/**
+ * Created by krrishnaaaa on September 12, 2016
+ */
+public class DemoException {
+
+    DemoException demo;
+
+    public static void main(String[] args) {
+        DemoException demoException = new DemoException();
+        System.out.println(demoException.getInt());
+    }
+
+    public int getInt() {
+        try {
+            System.out.println("try");
+            demo.printHello();
+            return 1;
+        } catch (NullPointerException ex) {
+            System.out.println("NullPointerException");
+            return 2;
+        } finally {
+            System.out.println("finally");
+            return 3;
+        }
+    }
+
+    public void printHello() {
+        System.out.println("hello");
+    }
+}
